6a4b463fe19aaa5dabe069d23d8540aa01bfe031
Chelsea Wetzel <chelswetz@gmail.com>
2020-06-12 15:25:54 -0400

    code cleanup

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,6 +1,3 @@
-from doubly_linked_list import DoublyLinkedList
-# from queue import Queue
-
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
@@ -9,6 +6,7 @@ class RingBuffer:
 
     def append(self, item):
         if len(self.storage) >= self.capacity:
+            # Overwrite oldest item
             self.storage[self.current_index] = item
             self.current_index = (self.current_index + 1) % self.capacity
         else:
@@ -17,26 +15,6 @@ class RingBuffer:
 
     def get(self):
         return self.storage
-
-
-# class RingBuffer:
-#     def __init__(self, capacity):
-#         self.capacity = capacity
-#         self.storage = DoublyLinkedList()
-
-#     def append(self, item):
-#         # if the length of the list is at max, remove item from tail
-#         # else add the item to the head of the list
-#         # FIFO
-#         if len(self.storage) >= self.capacity:
-#             self.storage.dequeue()
-#             # self.storage.storage.add_to_head(item)
-
-#         self.storage.enqueue(item)
-
-#     def get(self):
-#         return self.storage.storage.get_all_sorted()
-
 
 # A ring buffer is a non-growable buffer with a fixed size. 
 # When the ring buffer is full and a new element is inserted, 
